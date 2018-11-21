3b962ae300072da4821a9257ef9e869516767d90
Andrew Hu <andrewhu@nyu.edu>
2018-11-21 19:53:03 +0000

    Removed templates bc lucas is doing frontend

--- app.py
+++ app.py
@@ -12,10 +12,6 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
 mysql.init_app(app)
 
-# Home page where we'll display the site for now- maybe later we'll make a dedicated frontend
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():

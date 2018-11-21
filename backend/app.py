1d8e0b475e434d28a6bf395832e76683307a1f22
Andrew Hu <andrewhu@nyu.edu>
2018-11-21 21:04:38 +0000

    Added login

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,34 @@
-from flask import Flask, render_template
-from flaskext.mysql import MySQL
+from flask import Flask, request
+import pymysql
 from helpers import *
+import hashlib
 
 app = Flask(__name__)
 
-mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'suckingtoes'
-app.config['MYSQL_DATABASE_DB'] = 'pricosha'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.secret_key = 'super secret key!98nu9f8u2f'
 
-mysql.init_app(app)
+conn = pymysql.connect(host='localhost',
+                       user='user',
+                       password='haha',
+                       db='pricosha')
 
 
-@app.route('/login', methods=['GET', 'POST'])
+
+@app.route('/login', methods=['POST'])
 def login():
-    return "ok"	
+    # Info we get from the request
+    email = request.form['email']
+    password = request.form['password']
+
+    # Check the password hash
+    c = conn.cursor()
+    password_hash = hashlib.sha256(password.encode('utf8')).hexdigest()
+    sql = '''SELECT count(*) FROM Person WHERE email=%s AND password_hash=%s'''
+    c.execute(sql, (email, password_hash))
+    result = c.fetchone()
+    # Does not exist
+    if len(result)>0:
+        return 'login success'
+    else:
+        return 'login failed'
+

d73e04a3bd2adf9fe146aacabdf02cbe38231d7a
Andrew Hu <andrewhu@nyu.edu>
2018-12-14 23:21:47 +0000

    pushed

--- backend/app.py
+++ backend/app.py
@@ -34,23 +34,12 @@ def favicon():
     return send_file('img/favicon.ico')
 
 
-# 1. View public content posted in the last 24 hours
-# + Optional feature 5: Paginated results
-# Tested WORKING on 12/4
-@app.route('/posts/public')
-def public_content():
-    page = int(request.form.get('page',1))
-    results_per_page = int(request.form.get('results_per_page',10))
-    content = helpers.get_public_content(page,results_per_page)
-    return jsonify(content)
-
-
 # 2. Login - POST email and password
 # + Optional feature 1: User avatar. Avatar is url to static image. Avatars are public so no need to make private
 # Tested WORKING on 12/4
 @app.route('/login', methods=['POST'])
 def login():
-    req = request.get_json()['auth']
+    req = request.get_json()
 
     # Get info from request
     email = str(req['email'])
@@ -58,7 +47,6 @@ def login():
 
     # Attempt login
     login_ = helpers.get_login(email, password)
-
 
     if login_['success']:
         user = login_['response']
@@ -76,28 +64,42 @@ def login():
 def logout():
     # remove the username from the session if it's there
     session.pop('email', None)
-    return jsonify('logged out')
+    return jsonify(helpers.response(True, 'Logged out'))
 
 
 # Registers new user
 @app.route('/register', methods=['POST'])
 def register():
-    email = request.form['email']
-    password = request.form['password']
-    first_name = request.form['first_name']
-    last_name = request.form['last_name']
-    new_user = helpers.register(email, password, first_name, last_name)
+    req = request.get_json()
+    new_user = helpers.register(req['email'], req['password'], req['first_name'], req['last_name'])
     return jsonify(new_user)
 
 # Resets user password
 @app.route('/reset_password', methods=['POST'])
 def reset_password():
-    email = session['email']
-    old_password = request.form['old_password']
-    new_password = request.form['new_password']
-    pw_reset = helpers.reset_password(email, old_password, new_password)
+    req = request.get_json()
+    pw_reset = helpers.reset_password(session['email'], req['old_password'], req['new_password'])
     return jsonify(pw_reset)
 
+
+# 1. View public content posted in the last 24 hours
+# + Optional feature 5: Paginated results
+# Tested WORKING on 12/4
+@app.route('/posts/public')
+def public_content():
+    page = int(request.form.get('page',1))
+    results_per_page = int(request.form.get('results_per_page',10))
+    content = helpers.get_public_content(page,results_per_page)
+    return jsonify(content)
+
+
+@app.route('/posts/get')
+def get_post():
+    if 'email' not in session:
+        return 'User not logged in'
+    req = request.get_json()
+    post = helpers.get_post(req['item_id'])
+    return jsonify(post)
 
 # 3. View shared content items and info about them
 # + Optional feature 5: Paginated results
@@ -112,6 +114,76 @@ def get_shared_content():
     content = helpers.get_shared_content(email, page=page, results_per_page=results_per_page)
     return jsonify(content)
 
+
+# Optional feature 3: Saved posts. Gets post that user has saved
+# + Optional feature 5: Paginated results
+# Tested WORKING on 12/4
+@app.route('/posts/saved', methods=['GET', 'POST'])
+def get_saved_posts():
+    if 'email' not in session:
+        return 'User not logged in'
+    email = session['email']
+    page = request.form.get('page',1)
+    results_per_page = request.form.get('results_per_page',10)
+    saved_posts = helpers.get_saved_posts(email=email, page=page, results_per_page=results_per_page)
+    return jsonify(saved_posts)
+
+
+# Tested WORKING on 12/4
+@app.route('/posts/save', methods=['POST'])
+def save_post():
+    if 'email' not in session:
+        return 'User not logged in'
+    email = session['email']
+    item_id = request.form['item_id']
+    helpers.save_post(email, item_id)
+    return 'ok'
+
+
+# Tested WORKING on 12/4
+@app.route('/posts/unsave', methods=['POST'])
+def unsave_post():
+    if 'email' not in session:
+        return 'User not logged in'
+    email = session['email']
+    item_id = request.form['item_id']
+    helpers.unsave_post(email, item_id)
+    return 'ok'
+
+
+
+# 5. Post a content item
+# Optional feature 4: Post image content
+# Tested WORKING on 12/4
+@app.route('/posts/create', methods=['POST'])
+def create_post():
+    if 'email' not in session:
+        return 'User not logged in'
+    email = session['email']
+    item_name = request.form['item_name']
+    is_pub = request.form['is_pub']
+    filepath=None
+    if 'file' in request.files:
+        file = request.files['file']
+        if file.filename == '':
+            return jsonify('empty filename')
+        filepath = os.path.join('/home/user/pricosha/backend/img/', file.filename)
+        file.save(filepath)
+    result = helpers.create_content_item(email,item_name,is_pub,filepath)
+    return jsonify(result)
+
+
+# 6. Tag a content item
+# Tested WORKING on 12/4
+@app.route('/tags/create', methods=['POST'])
+def tag_content_item():
+    if 'email' not in session:
+        return 'User not logged in'
+    current_user = session['email']
+    tagee_email = request.form['tagee_email']
+    item_id = request.form['item_id']
+    tag_item = helpers.tag_item(current_user,tagee_email,item_id)
+    return jsonify(tag_item)
 
 # 4. Manage tags
 # 4a. Get proposed tags e.g. where tagee is user and status is false
@@ -138,41 +210,6 @@ def tags_modify():
     decision = request.form['decision']
     result = helpers.modify_proposed_tag(email_tagger, email_tagged, item_id, decision)
     return jsonify(result)
-
-
-# 5. Post a content item
-# Optional feature 4: Post image content
-# Tested WORKING on 12/4
-@app.route('/post/create', methods=['POST'])
-def post_create():
-    if 'email' not in session:
-        return 'User not logged in'
-    email = session['email']
-    item_name = request.form['item_name']
-    is_pub = request.form['is_pub']
-    filepath=None
-    if 'file' in request.files:
-        file = request.files['file']
-        if file.filename == '':
-            return jsonify('empty filename')
-        filepath = os.path.join('/home/user/pricosha/backend/img/', file.filename)
-        file.save(filepath)
-    result = helpers.create_content_item(email,item_name,is_pub,filepath)
-    return jsonify(result)
-
-
-# 6. Tag a content item
-# Tested WORKING on 12/4
-@app.route('/tags/create', methods=['POST'])
-def tag_content_item():
-    if 'email' not in session:
-        return 'User not logged in'
-    current_user = session['email']
-    tagee_email = request.form['tagee_email']
-    item_id = request.form['item_id']
-    helpers.tag_item(current_user,tagee_email,item_id)
-    return 'ok'
-
 
 # 7. Adds friend to friendgroup that person owns
 # Tested WORKING on 12/4
@@ -241,64 +278,33 @@ def set_profile_avatar():
     return jsonify(profile_info)
 
 
-# Optional feature 3: Saved posts. Gets post that user has saved
-# + Optional feature 5: Paginated results
-# Tested WORKING on 12/4
-@app.route('/get_saved_posts')
-def get_saved_posts():
-    if 'email' not in session:
-        return 'User not logged in'
-    email = session['email']
-    page = request.form.get('page',1)
-    results_per_page = request.form.get('results_per_page',10)
-    saved_posts = helpers.get_saved_posts(email=email, page=page, results_per_page=results_per_page)
-    return jsonify(saved_posts)
 
-
-# Tested WORKING on 12/4
-@app.route('/save_post', methods=['POST'])
-def save_post():
-    if 'email' not in session:
-        return 'User not logged in'
-    email = session['email']
-    item_id = request.form['item_id']
-    helpers.save_post(email, item_id)
-    return 'ok'
-
-
-# Tested WORKING on 12/4
-@app.route('/unsave_post', methods=['POST'])
-def unsave_post():
-    if 'email' not in session:
-        return 'User not logged in'
-    email = session['email']
-    item_id = request.form['item_id']
-    helpers.unsave_post(email, item_id)
-    return 'ok'
 
 # Optional feature 6: Add comments
 # Tested WORKING on 12/4
-@app.route('/post_comment', methods=['POST'])
+@app.route('/comments/post', methods=['POST'])
 def post_comment():
     if 'email' not in session:
         return 'User not logged in'
     email = session['email']
-    item_id = request.form['item_id']
-    comment = request.form['comment']
-    helpers.post_comment(email, item_id, comment)
-    return 'ok'
+    req = request.get_json()
+    item_id = req['item_id']
+    comment = req['comment']
+    new_comment = helpers.post_comment(email, item_id, comment)
+    return jsonify(new_comment)
 
 
-@app.route('/get_comments')
+@app.route('/comments/get')
 def get_comments():
+    req = request.get_json()
     email = session['email']
-    item_id = request.form['item_id']
+    item_id = req['item_id']
     comments = helpers.get_comments(email, item_id)
     return jsonify(comments)
 
 
-# deletes comment if user
-@app.route('/delete_comment', methods=['POST'])
+# deletes comment if user wrote comment or if user owns post
+@app.route('/comments/delete', methods=['POST'])
 def delete_comment():
     email = session['email']
     item_id = request.form['item_id']
